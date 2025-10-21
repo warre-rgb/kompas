47cf3ee9fc9ed12eafcb29fc46dcd50299448105
warre-rgb <warre@denheuvel.be>
2025-10-21 16:04:59 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,20 @@
+basic.forever(function () {
+    if (input.rotation(Rotation.Roll) < 5) {
+        basic.showLeds(`
+            # . . . #
+            # # . . #
+            # . # . #
+            # . . # #
+            # . . . #
+            `)
+    }
+    if (input.rotation(Rotation.Roll) < 335) {
+        basic.showLeds(`
+            # . . . #
+            # . . . #
+            # . # . #
+            # # . # #
+            # . . . #
+            `)
+    }
+})
